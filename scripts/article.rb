f0792a6a1f908bf35511b4c698ee06c31db20783
Esa Firman <esafirm21@gmail.com>
2019-12-31 11:54:04 +0700

    Update script

--- scripts/article.rb
+++ scripts/article.rb
@@ -9,20 +9,21 @@ if TITLE.empty?
   abort "Title should not be empty"
 end
 
+ARTICLE_DIR = "src/pages/articles"
 ARTICLE_DATE = `date -u +"%Y-%m-%dT%H:%M:%SZ"`.chomp
 FORMATTED_DATE = `date +%F`.chomp
-TEMPALTE_PATH = "temp---template/"
+TEMPALTE_PATH = "scripts/temp---template"
 
 NEW_PATH = "temp---#{DRAFT_KEY}---#{FORMATTED_DATE}---#{TITLE}".gsub(" ", "-")
 
 puts "new path #{NEW_PATH}"
 
+## Create content and move it
+`cp -r #{TEMPALTE_PATH} #{NEW_PATH}`
+`mv #{NEW_PATH} #{ARTICLE_DIR}`
+
 ## Go to the directory first
 Dir.chdir(ARTICLE_DIR)
-
-Dir.mkdir(NEW_PATH)
-
-`cp -r #{TEMPALTE_PATH} #{NEW_PATH}`
 
 TARGET = "#{NEW_PATH}/index.md"
 
