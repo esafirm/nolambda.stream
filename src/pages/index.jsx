896aaa6394aafedac9305dcd9ed276d69ffbf9a2
Esa Firman <esafirm21@gmail.com>
2019-12-31 11:49:29 +0700

    Fix build problem

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -69,6 +69,7 @@ export const pageQuery = graphql`
             categorySlug
           }
           frontmatter {
+            title
             date
             category
             description
