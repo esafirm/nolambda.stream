dff2a443d6b193756116129f9453caa806fde095
Esa Firman <esafirm21@gmail.com>
2023-04-19 09:15:22 +0700

    add typescript

---
+++ pages/components.tsx
@@ -0,0 +1,10 @@
+const ComponentConvert = () => {
+  return (
+    <div>
+      <h1>Component Convert</h1>
+      <p>Convert your components to pages</p>
+    </div>
+  )
+}
+
+export default ComponentConvert
---
+++ pages/hey.jsx
@@ -0,0 +1,11 @@
+import { CoolText } from '../components/CoolText'
+
+const Hey = () => {
+  return (
+    <div>
+      <CoolText />
+    </div>
+  )
+}
+
+export default Hey
